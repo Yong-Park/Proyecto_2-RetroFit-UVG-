8adf2ca01abefcff80532ed91028f63e8e77ee41
Yong <yong.park123456@gmail.com>
2021-04-27 13:15:39 -0600

    Terminado ya con funcionalidad

--- app/src/main/java/com/prueba/retrofit/MainActivity.kt
+++ app/src/main/java/com/prueba/retrofit/MainActivity.kt
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.appcompat.widget.SearchView
 import com.prueba.retrofit.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
 
     private lateinit var binding:ActivityMainBinding
     private lateinit var adapter: ArticleAdapter
@@ -20,9 +22,10 @@ class MainActivity : AppCompatActivity() {
 
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.searchNews.setOnQueryTextListener(this)
 
         intRecyclerView()
-        searchNew("general")
+        searchNew("business")
     }
 
     private fun intRecyclerView(){
@@ -35,13 +38,16 @@ class MainActivity : AppCompatActivity() {
         val api = Retrofit2()
 
         CoroutineScope(Dispatchers.IO).launch {
-            val call = api.getService()?.getNewsByCategory(country = "us",category, apiKey = "4b94054dbc6b4b3b9e50d8f62cde4f6c")
+            val call = api.getService()?.getNewsByCategory("us",category,"4b94054dbc6b4b3b9e50d8f62cde4f6c")
             val news: NewsResponse? = call?.body()
 
             runOnUiThread{
                 if(call!!.isSuccessful){
                     if(news?.status.equals("ok")){
-                        val articles = news?.articles ?: emptyList()
+                        val article = news?.articles ?: emptyList()
+                        articleList.clear()
+                        articleList.addAll(article)
+                        adapter.notifyDataSetChanged()
                     }else{
                         showMessage("Error en webservice")
                     }
@@ -55,5 +61,17 @@ class MainActivity : AppCompatActivity() {
     private fun showMessage(message:String) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        if(!query.isNullOrEmpty()){
+            searchNew(query.toLowerCase(Locale.ROOT))
+        }
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return true
+    }
+
 
 }

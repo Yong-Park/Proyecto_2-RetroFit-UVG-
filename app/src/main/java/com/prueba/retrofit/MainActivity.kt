d84cb93df4e6087ce819a4d057c78924b322edca
Yong <yong.park123456@gmail.com>
2021-05-04 11:12:23 -0600

    Es el mas reciente Pero los botones para cambiar de region no funcionan

--- app/src/main/java/com/prueba/retrofit/MainActivity.kt
+++ app/src/main/java/com/prueba/retrofit/MainActivity.kt
@@ -2,6 +2,7 @@ package com.prueba.retrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.appcompat.widget.SearchView
@@ -17,15 +18,46 @@ class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
     private lateinit var adapter: ArticleAdapter
     private val articleList = mutableListOf<Articles>()
 
+    private var place: String ="us"
+    private var type: String = "business"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        val btnUs: Button = findViewById(R.id.btnUs)
+        val btnCo: Button = findViewById(R.id.btnCo)
+        val btnRu: Button = findViewById(R.id.btnRu)
+        val btnCa: Button = findViewById(R.id.btnCa)
+
+        runSearch(type,place)
+
+        btnUs.setOnClickListener{
+            place = "us"
+            runSearch(type,place)
+        }
+        btnCo.setOnClickListener{
+            place = "co"
+            runSearch(type,place)
+        }
+        btnRu.setOnClickListener {
+            place = "ru"
+            runSearch(type,place)
+        }
+        btnCa.setOnClickListener {
+            place = "ca"
+            runSearch(type,place)
+        }
+
+    }
+
+    private fun runSearch(category:String, country:String){
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.searchNews.setOnQueryTextListener(this)
 
         intRecyclerView()
-        searchNew("business")
+        searchNew(type,place)
     }
 
     private fun intRecyclerView(){
@@ -34,11 +66,11 @@ class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
         binding.rvNews.adapter = adapter
     }
 
-    private fun searchNew(category:String){
+    private fun searchNew(category:String, country:String){
         val api = Retrofit2()
 
         CoroutineScope(Dispatchers.IO).launch {
-            val call = api.getService()?.getNewsByCategory("us",category,"4b94054dbc6b4b3b9e50d8f62cde4f6c")
+            val call = api.getService()?.getNewsByCategory(country,category,"4b94054dbc6b4b3b9e50d8f62cde4f6c")
             val news: NewsResponse? = call?.body()
 
             runOnUiThread{
@@ -64,7 +96,7 @@ class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
 
     override fun onQueryTextSubmit(query: String?): Boolean {
         if(!query.isNullOrEmpty()){
-            searchNew(query.toLowerCase(Locale.ROOT))
+            searchNew(query.toLowerCase(Locale.ROOT), query.toLowerCase(Locale.ROOT))
         }
         return true
     }
